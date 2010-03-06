cc959ad94d828a693ab6b0a633f3f00771340894
beepscore <support@beepscore.com>
2010-03-06 00:03:38 -0800

    Draw circular faces and arc smile and frown.

--- Classes/FrownView.m
+++ Classes/FrownView.m
@@ -19,43 +19,75 @@
     return self;
 }
 
-- (void)drawCircle {
+
+- (void)drawFrown:(CGPoint)faceCenter {
+    // create and build the path
+    CGMutablePathRef path = CGPathCreateMutable();
+    
+    const CGAffineTransform *m = NULL;
+    
+    CGPoint frownCenter;
+    frownCenter.x = faceCenter.x;
+    frownCenter.y = faceCenter.y + 60.0f;
+    const CGFloat frownRadius = 60.0f;
+    const CGFloat startAngle = 1.1f * M_PI;
+    const CGFloat endAngle = 1.9f * M_PI;
+    bool clockwise = false;
+    
+    CGPathAddArc(path, m, frownCenter.x, frownCenter.y, 
+                 frownRadius, startAngle, endAngle, clockwise);    
+    
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextAddPath(ctx, path);
+    
+    // configure the stroke parameters
+    CGContextSetStrokeColorWithColor(ctx, [UIColor blackColor].CGColor);
+    CGContextSetLineWidth(ctx, 5.0f);
+    // stroke the path
+    CGContextStrokePath(ctx);
+    
+    CGPathRelease(path);
+}
+
+
+- (void)drawFrownFace {
     // create and build the path
     CGMutablePathRef path = CGPathCreateMutable();
     
     const CGAffineTransform *m = NULL;
     CGPoint faceCenter;
-    faceCenter.x = 150.0f;
-    faceCenter.y = 140.0f;
+    faceCenter.x = 160.0f;
+    faceCenter.y = 300.0f;
     const CGFloat faceRadius = 120.0f;
     const CGFloat startAngle = 0.0f;
     const CGFloat endAngle = 2.0f * M_PI;
     bool clockwise = false;
-
+    
     CGPathAddArc(path, m, faceCenter.x, faceCenter.y, 
                  faceRadius, startAngle, endAngle, clockwise);    
-
+    
     CGPathCloseSubpath(path);
     CGContextRef ctx = UIGraphicsGetCurrentContext();
     CGContextAddPath(ctx, path);
-
+    
     // configure the fill parameters
-    CGContextSetFillColorWithColor(ctx, [UIColor yellowColor].CGColor);
-
+    CGContextSetFillColorWithColor(ctx, [UIColor purpleColor].CGColor);
+    
     // configure the stroke parameters
     CGContextSetStrokeColorWithColor(ctx, [UIColor blackColor].CGColor);
     CGContextSetLineWidth(ctx, 5.0f);
-
+    
     // fill and stroke the path
     CGContextDrawPath (ctx, kCGPathFillStroke);
-
+    
     CGPathRelease(path);
+    [self drawFrown:faceCenter];
 }
 
 
 - (void)drawRect:(CGRect)rect {
-    // Drawing code
-    [self drawCircle];
+    // Drawing code    
+    [self drawFrownFace];
 }
 
 
--- Classes/SmileyView.m
+++ Classes/SmileyView.m
@@ -20,34 +20,74 @@
 }
 
 
-- (void)drawSquare {
+- (void)drawSmile:(CGPoint)faceCenter {
     // create and build the path
     CGMutablePathRef path = CGPathCreateMutable();
-    CGPathMoveToPoint(path, NULL, 10.0f, 10.0f);
-    CGPathAddLineToPoint(path, NULL, 100.0f, 10.0f);
-    CGPathAddLineToPoint(path, NULL, 100.0f, 100.0f);
-    CGPathAddLineToPoint(path, NULL, 10.0f, 100.0f);
-    CGPathCloseSubpath(path);
+    
+    const CGAffineTransform *m = NULL;
+    
+    CGPoint smileCenter;
+    smileCenter.x = faceCenter.x;
+    smileCenter.y = faceCenter.y;
+    const CGFloat smileRadius = 60.0f;
+    const CGFloat startAngle = 0.1f * M_PI;
+    const CGFloat endAngle = 0.9f * M_PI;
+    bool clockwise = false;
+    
+    CGPathAddArc(path, m, smileCenter.x, smileCenter.y, 
+                 smileRadius, startAngle, endAngle, clockwise);    
+    
     CGContextRef ctx = UIGraphicsGetCurrentContext();
-    
-    // configure the fill parameters and fill the path
-    CGContextSetFillColorWithColor(ctx, [UIColor blueColor].CGColor);
     CGContextAddPath(ctx, path);
-    CGContextFillPath(ctx);
     
-    // configure the stroke parameters and stroke the path
-    CGContextSetStrokeColorWithColor(ctx, [UIColor yellowColor].CGColor);
-    CGContextSetLineWidth(ctx, 10.0f);
-    CGContextAddPath(ctx, path);
-    CGContextStrokePath(ctx);    
+    // configure the stroke parameters
+    CGContextSetStrokeColorWithColor(ctx, [UIColor blackColor].CGColor);
+    CGContextSetLineWidth(ctx, 5.0f);    
+    // stroke the path
+    CGContextStrokePath(ctx);
     
     CGPathRelease(path);
 }
 
 
+- (void)drawSmileyFace {
+    // create and build the path
+    CGMutablePathRef path = CGPathCreateMutable();
+    
+    const CGAffineTransform *m = NULL;
+    CGPoint faceCenter;
+    faceCenter.x = 160.0f;
+    faceCenter.y = 140.0f;
+    const CGFloat faceRadius = 120.0f;
+    const CGFloat startAngle = 0.0f;
+    const CGFloat endAngle = 2.0f * M_PI;
+    bool clockwise = false;
+    
+    CGPathAddArc(path, m, faceCenter.x, faceCenter.y, 
+                 faceRadius, startAngle, endAngle, clockwise);    
+    
+    CGPathCloseSubpath(path);
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextAddPath(ctx, path);
+    
+    // configure the fill parameters
+    CGContextSetFillColorWithColor(ctx, [UIColor yellowColor].CGColor);
+    
+    // configure the stroke parameters
+    CGContextSetStrokeColorWithColor(ctx, [UIColor blackColor].CGColor);
+    CGContextSetLineWidth(ctx, 5.0f);
+    
+    // fill and stroke the path
+    CGContextDrawPath (ctx, kCGPathFillStroke);
+    
+    CGPathRelease(path);
+    [self drawSmile:faceCenter];
+}
+
+
 - (void)drawRect:(CGRect)rect {
-    // Drawing code
-    [self drawSquare];
+    // Drawing code    
+    [self drawSmileyFace];
 }
 
 
